309751c4762f1e1d33b9720ec47ca71220019682
daniellebarrett <daniellebarrett@alumni.iu.edu>
2020-11-20 21:10:46 -0500

    Add project files.

---
+++ OP_Challenge_Loops_Conditions/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OP_Challenge_Loops_Conditions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OP_Challenge_Loops_Conditions")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("54823c0c-11ff-497f-8435-335c8506546b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ OP_Challenge_Loops_Conditions/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OP_Challenge_Loops_Conditions
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            //1 print every letter of this word to the console one at a time
+            //Could also set this up as 2 below(minus the if else statements
+             string value = "Supercalifragilisticexpialidocious";
+             for (int i =0; i <value.Length; i++)
+             {
+                 Console.WriteLine(value[i]);
+             }  
+             
+
+             //2 now only print the letter if its an 'i', if its any other letter print 'not an i'
+            string word = "Supercalifragilisticexpialidocious";
+            foreach (char c in word)
+                if (c == 'i')
+                { Console.WriteLine(c); }
+                else if (c == 'l')      //another bonus print letter if it is L
+                { Console.WriteLine(c); }
+                else
+                { Console.WriteLine("Not an I"); }
+
+            //bonus: print num of letters in the word
+            int count = word.Length;
+            Console.WriteLine(count);
+            
+                
+
+
+           
+        
+        }
+    }
+}
